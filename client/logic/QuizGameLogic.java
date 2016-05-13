f8ccb686197608710abf6e149e7f9785d48211e6
genesis2031 <genesis2031@gmail.com>
2016-05-13 21:26:17 +0200

    logic adattagok

--- client/logic/QuizGameLogic.java
+++ client/logic/QuizGameLogic.java
@@ -1,7 +1,7 @@
 public class QuizGameLogic {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+	private String question;
+	private String[] answers;
+	private Frame gui;
 	
 }
