853117566fd828fbfc3ee3e89140299e4cc2c648
anystone1234 <proxie@caesar.elte.hu>
2016-05-13 15:59:10 +0200

    Gomb megvalósítása

--- client/gui/Button.java
+++ client/gui/Button.java
@@ -1,5 +1,22 @@
-package projekt;
+package client.gui;
 
-public class Button {
+import javax.swing.JButton;
+
+public class Button extends JButton{
+    
+    private int azonosito;
+    
+    public Button(String szoveg , int azonosito){
+        super(szoveg);
+        this.azonosito=azonosito;
+    }
+    
+    public void setAzonosito(int azon){
+        this.azonosito=azon;
+    }
+    
+    public int getAzonosito(){
+        return this.azonosito;
+    }
     
 }
