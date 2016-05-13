2a85ab747d492f731f85acdb2f674f93aa03155f
genesis2031 <genesis2031@gmail.com>
2016-05-13 21:35:32 +0200

    logic method declarations

--- client/logic/QuizGameLogic.java
+++ client/logic/QuizGameLogic.java
@@ -2,6 +2,27 @@ public class QuizGameLogic {
 
 	private String question;
 	private String[] answers;
+	private int correctIndex;
 	private Frame gui;
 	
+	public QuizGameLogic(Frame gui) {
+		this.gui = gui;
+	}
+	
+	private String getQuestionFromServer() {
+		//...
+		return question;
+	}
+	
+	private void setQuestionInGui() {
+		//...	
+	}
+	
+	private void gameQuestionSetup() {
+		//...
+	}
+	
+	public boolean isCorrectAnswer(int i) {
+		//...
+	}
 }
